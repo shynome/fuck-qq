9d70144eade0c786bdf948f9e2c0fc1694f2cd03
shynome <shynome@gmail.com>
2024-01-04 00:49:01 +0800

    在 Docker 中运行时会启动 copyq

--- main.go
+++ main.go
@@ -1,11 +1,24 @@
 package main
 
 import (
+	"os"
+	"os/exec"
+
 	"github.com/labstack/echo/v4"
+	"github.com/shynome/err0/try"
 	"github.com/shynome/fuck-qq/onebot"
 )
 
 func main() {
+	if os.Getenv("RUN_COPYQ") != "" {
+		go func() {
+			cmd := exec.Command("copyq")
+			cmd.Stderr = os.Stderr
+			cmd.Stdout = os.Stdout
+			try.To(cmd.Start())
+			try.To(cmd.Wait())
+		}()
+	}
 	e := echo.New()
 	onebot.Inject(e)
 	e.Start(":5700")
